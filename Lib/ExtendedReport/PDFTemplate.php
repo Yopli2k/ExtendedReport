306c8de3e8575aff14840ff48ba505fba9993676
José Antonio Cuello Principal <yopli2000@gmail.com>
2022-03-04 15:03:23 +0100

    Update licence

--- Lib/ExtendedReport/PDFTemplate.php
+++ Lib/ExtendedReport/PDFTemplate.php
@@ -2,9 +2,20 @@
 /**
  * This file is part of ExtendedReport plugin for FacturaScripts.
  * FacturaScripts Copyright (C) 2015-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
- * ExtendedReport Copyright (C) 2020-2022 Jose Antonio Cuello Principal <yopli2000@gmail.com>
+ * ExtendedReport Copyright (C) 2021-2022 Jose Antonio Cuello Principal <yopli2000@gmail.com>
  *
- * This program and its files are under the terms of the license specified in the LICENSE file.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 namespace FacturaScripts\Plugins\ExtendedReport\Lib\ExtendedReport;
 
@@ -15,7 +26,7 @@ use FacturaScripts\Plugins\ExtendedReport\Lib\WidgetReport\ReportItemLoadEngine;
 use FacturaScripts\Plugins\ExtendedReport\Model\Base\ModelReport;
 
 /**
- * Description of PDFTemplate
+ * Main class for generate PDF report from XML Report file.
  *
  * @author Jose Antonio Cuello Principal <yopli2000@gmail.com>
  */
--- Lib/WidgetReport/ColumnItem.php
+++ Lib/WidgetReport/ColumnItem.php
@@ -1,10 +1,21 @@
 <?php
 /**
  * This file is part of ExtendedReport plugin for FacturaScripts.
- * FacturaScripts Copyright (C) 2018-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
- * ExtendedReport Copyright (C) 2018-2022 Jose Antonio Cuello Principal <yopli2000@gmail.com>
+ * FacturaScripts Copyright (C) 2015-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * ExtendedReport Copyright (C) 2021-2022 Jose Antonio Cuello Principal <yopli2000@gmail.com>
  *
- * This program and its files are under the terms of the license specified in the LICENSE file.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 namespace FacturaScripts\Plugins\ExtendedReport\Lib\WidgetReport;
 
