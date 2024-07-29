2781d84faa88d7627e7d5cd7a3ca8c54393b2fec
José Antonio Cuello Principal <yopli2000@gmail.com>
2024-07-29 09:15:33 +0200

    update licence

--- Lib/ExtendedReport/PDFTemplate.php
+++ Lib/ExtendedReport/PDFTemplate.php
@@ -5,16 +5,16 @@
  * ExtendedReport Copyright (C) 2021-2024 Jose Antonio Cuello Principal <yopli2000@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
+ * it under the terms of the GNU Lesser General Public license as
  * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * license, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * GNU Lesser General Public license for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU Lesser General Public license
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 namespace FacturaScripts\Plugins\ExtendedReport\Lib\ExtendedReport;
